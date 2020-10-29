09e9d78efc3bb6bf561984268bed7f3e66d903e5
Napo <Michaeljnapolitano@gmail.com>
2020-10-29 10:37:08 -0400

    set up product and category models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'product_tag',
+})
 
 // Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(Product, {
+  foreignKey: 'product_tag',
+})
 
 module.exports = {
   Product,
